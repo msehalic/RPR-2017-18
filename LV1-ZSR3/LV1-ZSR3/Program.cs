ca7d60e831fa6aabe8f15b0d3322ebb9814d0d48
msehalic <msehalic1@etf.unsa.ba>
2017-10-16 21:11:53 +0200

    up
    MiniSah i dalje buggy

---
+++ LV1-ZSR3/LV1-ZSR3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV1_ZSR3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite broj n: ");
+        }
+    }
+}
---
+++ MiniSah/MiniSah/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniSah
+{
+
+   class Figura
+    {
+        public enum Potez { NaprijedzaN = 1, Naprijed = 2, KonjLijevo = 3, KonjDesno = 4 };
+        string pomjeri(int potez)
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ulaz;
+            Console.Write("Dobro dosli u Mini Sah. Molimo odaberite opciju 1 ili 2: ");
+            ulaz = Console.ReadLine();
+        }
+    }
+}
