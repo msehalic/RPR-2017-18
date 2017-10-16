3abd1c3ba09e6298a93e40fb1524e6f991fa6502
msehalic <msehalic1@etf.unsa.ba>
2017-10-16 20:30:36 +0200

    ZSR5

---
+++ LV1-ZSR5/LV1-ZSR5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV1_ZSR5
+{
+    class Program
+    {
+        static string OkreniGa (string s)
+        {
+            char[] niz = s.ToCharArray();
+            Array.Reverse(niz);
+            return new string(niz);
+        }
+        static bool DaLiJePalindrom(string s)
+        {
+            string trim = s.Replace(" ", "");
+            trim=trim.ToLower();
+            string poredbeni = OkreniGa(trim);
+            return (poredbeni==trim);
+        }
+        static void Main(string[] args)
+        {
+            string s;
+            Console.Write("Unesite neki string: ");
+            s = Console.ReadLine();
+            string palindrom = DaLiJePalindrom(s) ? "je" : "nije";
+            Console.WriteLine("Uneseni string " + palindrom + " palindrom");
+        }
+    }
+}
