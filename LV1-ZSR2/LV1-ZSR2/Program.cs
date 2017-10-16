2b605c555222490b639331f6118ce1811d5d3582
msehalic <msehalic1@etf.unsa.ba>
2017-10-16 20:05:45 +0200

    LV1-ZSR2

---
+++ LV1-ZSR2/LV1-ZSR2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV1_ZSR2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Unesite n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int m = n;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j < m; j++)
+                    Console.Write(" ");
+                m--;
+                for (int j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+    }
+}
