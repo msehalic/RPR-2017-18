3f5c6adc4c086c1da6b1b68fd49d67d6163e362b
msehalic <msehalic@users.noreply.github.com>
2017-10-19 14:45:05 +0200

    LV1-ZSR6

---
+++ LV1/ZSR1-6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZSR1_6
+{
+    class Program
+    {
+        static string IzbrisiPodstring(string prvi, string drugi)
+        {
+            return prvi.Replace(drugi, ""); ;
+        }
+        static int NadjiPodstring(string prvi, string drugi)
+        {
+            return prvi.IndexOf(drugi);
+        }
+        static void Main(string[] args)
+        {
+            string prvi = "Abcdefgh", drugi = "def";
+            int n = NadjiPodstring(prvi, drugi);
+            prvi = IzbrisiPodstring(prvi, drugi);
+            Console.WriteLine("Drugi string se nalazi na poziciji {0}", n);
+            Console.WriteLine("String nakon izbacivanja glasi {0}", prvi);
+        }
+    }
+}
