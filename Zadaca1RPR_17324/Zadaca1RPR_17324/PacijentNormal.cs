d31dd1eae7641a0660b2120c68f0d41bec752bf9
msehalic <msehalic1@etf.unsa.ba>
2017-11-03 22:52:41 +0100

    up

---
+++ Zadaca1RPR_17324/Zadaca1RPR_17324/PacijentNormal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaca1RPR_17324
+{
+    class PacijentNormal:Pacijent
+    {
+        //TODO, TREBA SAMO POKUPITI SVE IZ BAZNOG JER JE STANDARDNI POSTUPAK, PRIPAZITI NA ONAJ WEIRD HITNI PROCEDURE :)
+    }
+}
