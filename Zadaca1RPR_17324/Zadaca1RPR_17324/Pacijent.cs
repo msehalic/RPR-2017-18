2004de2726474d9134bc769ea12addc037b5c535
msehalic <msehalic1@etf.unsa.ba>
2017-11-03 14:51:03 +0100

    update

---
+++ Zadaca1RPR_17324/Zadaca1RPR_17324/Pacijent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaca1RPR_17324
+{
+    abstract class Pacijent
+    {
+
+    }
+}
