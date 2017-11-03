75d5a027a05e91fedbf645603840698e531f089f
msehalic <msehalic1@etf.unsa.ba>
2017-11-03 17:31:09 +0100

    a

---
+++ Zadaca1RPR_17324/Zadaca1RPR_17324/PacijentHitno.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaca1RPR_17324
+{
+    class PacijentHitno : Pacijent
+    {
+        bool PacijentZiv { get; set; }
+        string EvidentirajPrvuPomoc(Pacijent p)
+        {
+            string vrijemeSmrti, razlogSmrti, postupak;
+            char prezivio;
+            Console.WriteLine("Kojem postupku je podvrgnut pacijent?");
+            postupak = Console.ReadLine();
+            //i ovdje nekako u karton zgurati
+            Console.WriteLine("Pacijent je ziv (D/N)?");
+            prezivio = Convert.ToChar(Console.ReadLine());
+            PacijentZiv= (prezivio == 'D');
+            if (PacijentZiv) return ("Uspjesno obavljen postupak" + postupak);
+            else
+            {
+                Console.Write("Unesite vrijeme smrti: ");
+                vrijemeSmrti = Console.ReadLine();
+                Console.Write("\nUnesite razlog smrti: ");
+                razlogSmrti = Console.ReadLine();
+                //treba ovo zapisati negdje, mozda neka klasa pregleda pa u listu da se zapisuju pregledi i komentari tj. karton pacijenta
+                return ("Pacijent je nazalost preminuo u" + vrijemeSmrti + "zbog" + razlogSmrti);
+            }
+        }
+    }
+}
