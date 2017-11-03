87fe316ef77f6d30937e900ea8578ab5729151fa
msehalic <msehalic1@etf.unsa.ba>
2017-11-03 14:41:35 +0100

    Zadaca

---
+++ Zadaca1RPR_17324/Zadaca1RPR_17324/IGlavni.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaca1RPR_17324
+{
+    interface IGlavni
+    {
+        void RegistrujPacijenta();
+        decimal IznosPlacanja();
+    }
+}
---
+++ Zadaca1RPR_17324/Zadaca1RPR_17324/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaca1RPR_17324
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
