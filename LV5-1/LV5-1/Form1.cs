e8b3ac77266fb1158b34d5c4c622202ad99cd911
msehalic <msehalic@users.noreply.github.com>
2017-11-16 15:50:31 +0200

    menadzment

---
+++ LV5-1/LV5-1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LV5_1
+{
+    public partial class FormMenadzmentKorisnika : Form
+    {
+        public FormMenadzmentKorisnika()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPonisti_Click(object sender, EventArgs e)
+        {
+            textBoxIme.Text = "";
+            textBoxPrezime.Text = "";
+            radioButtonMusko.Checked = false;
+            radioButtonZensko.Checked = false;
+            comboBoxGrad.ResetText();
+            dateTimePickerDatumRodjenja.ResetText();
+            maskedTextBoxTelefon.ResetText();
+            textBoxKorisnickoIme.Text = "";
+            textBoxLozinka.Text = "";
+            pictureBoxSlikaKorisnika.ResetText();
+            checkBoxAdmin.Checked = false;
+            checkBoxModerator.Checked = false;
+            textBoxPretragaIme.Text = "";
+            richTextBoxPretraga.Text = "";
+        }
+        private void buttonUcitavanjeSlike_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Izaberite sliku"; dlg.Filter = "jpg files (*.jpg)|*.jpg";
+
+                if (dlg.ShowDialog() == DialogResult.OK) { pictureBoxSlikaKorisnika.Image = new Bitmap(dlg.FileName); }
+            }
+        }
+    }
+}
