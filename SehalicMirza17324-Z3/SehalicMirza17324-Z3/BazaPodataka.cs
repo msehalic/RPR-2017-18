182d510c2f0dc641b6eb032d51498ba16132029b
msehalic <msehalic1@etf.unsa.ba>
2018-01-17 11:34:27 +0100

    up

--- SehalicMirza17324-Z3/SehalicMirza17324-Z3/BazaPodataka.cs
+++ SehalicMirza17324-Z3/SehalicMirza17324-Z3/BazaPodataka.cs
@@ -1,6 +1,9 @@
 ﻿using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,5 +36,139 @@ namespace SehalicMirza17324_Z2
 
         }
         public void DiskonektujSe() => spajanjeSaBazom.Close();
+
+        private void UcitajUposlenike()
+        {
+            using (OracleCommand cmd = spajanjeSaBazom.CreateCommand())
+            {
+                cmd.CommandText = "SELECT * FROM Uposlenik u, Administrator a WHERE u.brojLicence = a.brojLicence";
+                OracleDataAdapter adp = new OracleDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adp.Fill(ds);
+                klinika17324.Uposlenici.Clear();
+
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    Image Slika;
+                    byte[] Bajtovi = row["slikaUposlenika"] as byte[];
+                    if (Bajtovi == null)
+                    {
+                        Slika = null;
+                    }
+                    else
+                    {
+                        using (var stream = new MemoryStream(Bajtovi))
+                        {
+                            Bitmap bmp = new Bitmap(stream);
+                            Slika = bmp;
+                        }
+                    }
+                    klinika17324.Uposlenici.Add(new Doktori.Administrator(row["imeUposlenika"].ToString(), row["prezimeUposlenika"].ToString(),
+                       Slika, Convert.ToInt32(row["brojLicence"]), row["korisnickoIme"].ToString(), row["lozinka"].ToString()));
+                    klinika17324.Uposlenici.Last().Lozinka = row["lozinka"].ToString();
+                    klinika17324.Uposlenici.Last().BrojLicence = Int32.Parse(row["brojLicence"].ToString());
+                }
+            }
+            using (OracleCommand cmd = spajanjeSaBazom.CreateCommand())
+            {
+                cmd.CommandText = "SELECT * FROM Uposlenik u, Tehnicar t WHERE u.brojLicence = t.brojLicence";
+                OracleDataAdapter adp = new OracleDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adp.Fill(ds);
+                klinika17324.Uposlenici.Clear();
+
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    Image Slika;
+                    byte[] Bajtovi = row["slikaUposlenika"] as byte[];
+                    if (Bajtovi == null)
+                    {
+                        Slika = null;
+                    }
+                    else
+                    {
+                        using (var stream = new MemoryStream(Bajtovi))
+                        {
+                            Bitmap bmp = new Bitmap(stream);
+                            Slika = bmp;
+                        }
+                    }
+                    klinika17324.Uposlenici.Add(new Doktori.Tehnicar(row["imeUposlenika"].ToString(), row["prezimeUposlenika"].ToString(),
+                       Slika, Convert.ToInt32(row["brojLicence"]), row["korisnickoIme"].ToString(), row["lozinka"].ToString()));
+                    klinika17324.Uposlenici.Last().Lozinka = row["lozinka"].ToString();
+                    klinika17324.Uposlenici.Last().BrojLicence = Int32.Parse(row["brojLicence"].ToString());
+                }
+            }
+
+
+            using (OracleCommand cmd = spajanjeSaBazom.CreateCommand())
+            {
+                cmd.CommandText = "SELECT * FROM Uposlenik u, Doktor d WHERE u.brojLicence = d.brojLicence";
+                OracleDataAdapter adp = new OracleDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adp.Fill(ds);
+                klinika17324.Uposlenici.Clear();
+
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    Image Slika;
+                    byte[] Bajtovi = row["slikaUposlenika"] as byte[];
+                    if (Bajtovi == null)
+                    {
+                        Slika = null;
+                    }
+                    else
+                    {
+                        using (var stream = new MemoryStream(Bajtovi))
+                        {
+                            Bitmap bmp = new Bitmap(stream);
+                            Slika = bmp;
+                        }
+                    } 
+                    klinika17324.Uposlenici.Add(new Doktori.Doktor(row["imeUposlenika"].ToString(), row["prezimeUposlenika"].ToString(),
+                       Slika, Convert.ToInt32(row["brojLicence"]), row["korisnickoIme"].ToString(), row["lozinka"].ToString(), Convert.ToInt32(row["brojPregledanihPacijenata"]), row["specijalizacija"].ToString()));
+                    klinika17324.Uposlenici.Last().Lozinka = row["lozinka"].ToString();
+                    klinika17324.Uposlenici.Last().BrojLicence = Int32.Parse(row["brojLicence"].ToString());                   
+                }
+            }
+                   
+        }
+        private void SacuvajUposlenike()
+        {
+            //admin
+            foreach (Doktori.Administrator a in klinika17324.Uposlenici)
+            {
+                using (OracleCommand cmd = spajanjeSaBazom.CreateCommand())
+                {
+                    string sqlInsert = String.Format("insert into Administrator (brojLicence, imeUposlenika, prezimeUposlenika, slikaUposlenika, korisnickoIme, lozinka) values " +
+                        "({0}, '{1}', {2}, {3}, {4}, {5})", a.BrojLicence, a.ImeUposlenika, a.PrezimeUposlenika, a.SlikaUposlenika, a.KorisnickoIme, a.Lozinka);
+                    cmd.CommandText = sqlInsert;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            //tehnicari
+            foreach (Doktori.Tehnicar t in klinika17324.Uposlenici)
+            {
+                using (OracleCommand cmd = spajanjeSaBazom.CreateCommand())
+                {
+                    string sqlInsert = String.Format("insert into Tehnicar (brojLicence, imeUposlenika, prezimeUposlenika, slikaUposlenika, korisnickoIme, lozinka) values " +
+                        "({0}, '{1}', {2}, {3}, {4}, {5})", t.BrojLicence, t.ImeUposlenika, t.PrezimeUposlenika, t.SlikaUposlenika, t.KorisnickoIme, t.Lozinka);
+                    cmd.CommandText = sqlInsert;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            //doktori
+            foreach (Doktori.Doktor d in klinika17324.Uposlenici)
+            {
+                using (OracleCommand cmd = spajanjeSaBazom.CreateCommand())
+                {
+                    string sqlInsert = String.Format("insert into Doktor (brojLicence, imeUposlenika, prezimeUposlenika, slikaUposlenika, korisnickoIme, lozinka, brojPregledanihPacijenata, specijalizacija) values " +
+                        "({0}, '{1}', {2}, {3}, {4}, {5}, {6}, {7})", d.BrojLicence, d.ImeUposlenika, d.PrezimeUposlenika,d.SlikaUposlenika, d.KorisnickoIme, d.Lozinka, d.BrojPregledanihPacijenata.ToString(), d.Specijalizacija);
+                    cmd.CommandText = sqlInsert;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
+
 }
